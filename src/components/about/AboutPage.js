e8ebfcc997b4100b9748545c6f0e4afb69cecdb1
Xander Harris <xanderh@hotmail.co.uk>
2017-11-23 10:16:45 -0800

    adding routes and pages

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class AboutPage extends React.Component {
+    render() {
+        return (
+            <div className="jumbotron">
+                <h1>About</h1>
+                <p>This is a React/Redux App to learn dev skills</p>
+            </div>
+        );
+    }
+}
+
+export default AboutPage;
---
+++ src/components/course/CoursesPage.js
@@ -0,0 +1,18 @@
+import React, {PropTypes} from 'react';
+// import { bindActionCreators } from 'redux';
+// import {connect} from 'react-redux';
+// import {browserHistory} from 'react-router';
+// //import * as courseActions from '../../actions/courseActions';
+// import CourseList from './CourseList';
+
+class CoursesPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Courses</h1>
+      </div>
+    );
+  }
+}
+
+export default CoursesPage;
