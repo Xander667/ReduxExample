9fad5fd0ae54793f4a507f201474fb3f70cedc32
David Acheson <dacheson@microsoft.com>
2017-11-28 11:42:50 -0800

    cleanup  files

--- src/components/compareTelephone/CompareTelephonePage.test.js
+++ src/components/compareTelephone/CompareTelephonePage.test.js
@@ -1,46 +1,69 @@
 import expect from 'expect';
 
-    /// Add syntax
-    function compareTelephoneNumbers(number1, number2) {
-        if(!number1 || !number2) { return ('False');}
+const decimalBase = 10;
+
+    /**
+     * Compare two strings and verify if the numbers are the same, all else ignored.
+     * @param phoneNumber1 first telephone number string
+     * @param phoneNumber2 second telephone number string
+     * @returns True or False depending on if the numbers are the same
+     */
+    function compareTelephoneNumbers(phoneNumber1, phoneNumber2) {
+        if (!phoneNumber1 || !phoneNumber2) { return ('False'); }
     
+
+        // while loop both
+
+        //     while find numberA
+
+        //     while find numberB
+
+        //     compare
+    
+
+
         let innerPointer = 0;
-        for(let index=0; index<number1.length; index++) {
-          if(parseInt(number1[index], 10)) {  //const for 10
-  
-            for(let innerIndex=innerPointer; innerIndex<number2.length; innerIndex++) {
-              if(parseInt(number2[innerIndex], 10)) {
-                if(number1[index] !== number2[innerIndex]) { 
-                  return 'False'; 
-                } else {
-                  innerPointer = ++innerIndex;
-                  break;
+        for (let index = 0; index < phoneNumber1.length; index++) {
+            if (parseInt(phoneNumber1[index], decimalBase)) {
+    
+                for (let innerIndex = innerPointer; innerIndex < phoneNumber2.length; innerIndex++) {
+                    if (parseInt(phoneNumber2[innerIndex], decimalBase)) { //is NaN
+                        if (phoneNumber1[index] !== phoneNumber2[innerIndex]) {
+                            return 'False';
+                        } else {
+                            innerPointer = ++innerIndex;
+                            break;
+                        }
+                    }
                 }
-              } 
             }
-          }
         }
     
         return 'True';
     }
-  
 
-describe('compareTelephoneNumbers tests', () => {
+describe('compareTelephoneNumbers input tests', () => {
 
-    it('same numbers', () => {
+    it('Number A and B are identical numbers', () => {
         expect(compareTelephoneNumbers('333','333')).toEqual('True');
     });
 
-    it('A longer', () => {
+    it('Number A is longer than Number B', () => {
         expect(compareTelephoneNumbers('333ee','333')).toEqual('True');
     });
 
-    it('B longer', () => {
+    it('Number B is longer than Number A', () => {
         expect(compareTelephoneNumbers('333','ee333')).toEqual('True');
     });
 
-    it('diff numbers', () => {
+    it('Number A and Number B are different', () => {
         expect(compareTelephoneNumbers('4233','333')).toEqual('False');
+        expect(compareTelephoneNumbers('','333')).toEqual('False');
+        expect(compareTelephoneNumbers(null, '333')).toEqual('False');
+        expect(compareTelephoneNumbers('3r2','')).toEqual('False');
+        expect(compareTelephoneNumbers('fgreer', null)).toEqual('False');
+        expect(compareTelephoneNumbers('Agreer', null)).toEqual('False');
+        expect(compareTelephoneNumbers('***greer', null)).toEqual('False');
     });
 
     it('Lots of jibberish numbers?', () => {
